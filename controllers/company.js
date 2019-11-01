eb60c564ae025555e72329f2eefd7bdabc482b6f
adwaith <adwaith@qburst.com>
2019-11-01 17:46:10 +0530

    # Completed company CRUD.

--- controllers/company.js
+++ controllers/company.js
@@ -4,7 +4,7 @@ const {throwError, throwIf, sendSuccess, sendError} = require("../helpers").resp
 module.exports.createCompany = async (req, res, next) => {
     try{
         company = await Company.create({name: req.body.name}).then(
-            throwIf(r => !r, 400, 'could not create user ', 'User Not Create'),
+            throwIf(r => !r, 400, 'could not create company ', 'Company Not Created'),
             throwError(500, 'sequelize error')
         )
         sendSuccess(res, 'Company Created')({company})
@@ -17,7 +17,7 @@ module.exports.readCompany = async (req, res, next) => {
     const companyId = req.params.id
     try{
         company = await Company.findByPk(companyId).then(
-            throwIf(r => !r, 400, 'not found', 'User Not Found'),
+            throwIf(r => !r, 400, 'not found', 'Company Not Found'),
             throwError(500, 'sequelize error')
         )
         sendSuccess(res, 'Company Found')({company})
@@ -26,6 +26,38 @@ module.exports.readCompany = async (req, res, next) => {
     }
 };
 
-module.exports.updateCompany = (req, res, next) => {};
+module.exports.updateCompany = async (req, res, next) => {
+    const companyId = req.params.id;
+    const companyName = req.body.name;
+    try{
+        company = await Company.findByPk(companyId).then(
+            throwIf(r => !r, 400, 'not found', 'Company Not Found'),
+            throwError(500, 'sequelize error')
+        )
+        updatedCompany = await company.update({name: companyName}).then(
+            throwIf(r => !r, 400, 'not updated', 'Company Not Updated'),
+            throwError(500, 'sequelize error')
+        )
+        sendSuccess(res, 'Company Updated')({updatedCompany})
+    }catch(error) {
+        sendError(res)(error)
+    }
+};
 
-module.exports.deleteCompany = (req, res, next) => {};
+module.exports.deleteCompany = async (req, res, next) => {
+    const companyId = req.params.id;
+    try{
+        company = await Company.findByPk(companyId).then(
+            throwIf(r => !r, 400, 'not found', 'Company Not Found'),
+            throwError(500, 'sequelize error')
+        )
+        company = await company.destroy().then(
+            throwIf(r => !r, 400, 'not deleted', 'Company Not Deleted'),
+            throwError(500, 'sequelize error')
+        )
+        sendSuccess(res, 'Company Deleted')({company})
+    }
+    catch(error){
+        sendError(res)(error)
+    }
+};
--- routes/company.js
+++ routes/company.js
@@ -8,11 +8,11 @@ router.post("/create", companyController.createCompany);
 // /companies/:id       -- Read
 router.get("/:id", companyController.readCompany);
 
-// /company/:id         -- Update
+// /companies/:id         -- Update
 router.put("/:id", companyController.updateCompany);
 router.patch("/:id", companyController.updateCompany);
 
-// /company/:id         -- Delete
+// /companies/:id         -- Delete
 router.delete("/:id", companyController.deleteCompany);
 
 module.exports = router;
